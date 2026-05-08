eb5260e46981b26922173dd2472e2feb0f590f74
Nathanael Yusuf Tjahjanadi <nathanaelytj@ntj.co.id>
2026-05-08 23:31:58 +0000

    [ADD] bulk forward-port of 26 missing modules from 18.0 to 19.0
    OCA upstream 19.0 doesn't ship these; structural baseline for
    further v19 audit + fixes (per-module).
    
    Tier-validation children (5):
      base_tier_validation_confirm_auth
      base_tier_validation_correction
      base_tier_validation_formula
      base_tier_validation_forward
      base_tier_validation_server_action
    
    Server-action / wizard / mass-edit (5):
      multi_step_wizard
      server_action_mass_edit
      server_action_mass_edit_onchange
      template_content_swapper
      chained_swapper
    
    Base utilities (10):
      announcement
      barcode_action
      base_custom_filter
      base_export_manager
      base_import_security_group
      base_menu_visibility_restriction
      base_optional_quick_create
      base_warn_option
      default_multi_user
      developer_menu
    
    Filter / sequence / document (6):
      date_range_account
      document_quick_access
      document_quick_access_folder_auto_classification
      filter_multi_user
      sequence_check_digit
      sequence_reset_period
    
    All bumped to 19.0.1.0.0 manifest; NTJ copyright added.
    Source ref: 18.0. Includes test-requirements.txt copied as-is.
    
    WIP: per-module v19 audits to follow.

---
+++ sequence_check_digit/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2017 Creu Blanca
+# Copyright 2026 NTJ (https://www.ntj.co.id)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Check Digit on Sequences",
+    "version": "19.0.1.0.0",
+    "category": "Reporting",
+    "website": "https://github.com/OCA/server-ux",
+    "author": "Creu Blanca, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Adds a check digit on sequences",
+    "depends": ["base"],
+    "data": ["views/sequence_views.xml"],
+}
